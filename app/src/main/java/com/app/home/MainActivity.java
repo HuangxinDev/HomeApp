a6ac7ff78ab3bdbc7cf201e682d8637c3ad159c8
sunnyXin88 <huang_xin88@163.com>
2017-08-09 00:02:17 +0800

    feat(20170808002): 添加greendao，eventbus，fastjson第三方jar配置

--- app/src/main/java/com/app/home/MainActivity.java
+++ app/src/main/java/com/app/home/MainActivity.java
@@ -3,11 +3,45 @@ package com.app.home;
 import android.app.Activity;
 import android.os.Bundle;
 
+import org.greenrobot.eventbus.EventBus;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
