a4bdebb4d5e38dfdd2281ec5fdf6010c4a8498bd
sunnyXin88 <huang_xin88@163.com>
2017-12-01 22:18:23 +0800

    fix(src): remove unused description
    N/A
    
    BREAKING CHANGE: N/A
    
    Closes N/A
    
    Signed-off-by: sunnyXin88 <huang_xin88@163.com>

---
+++ app/src/main/java/com/app/home/app/bean/SimpleData.java
@@ -0,0 +1,22 @@
+package com.app.home.app.bean;
+
+public class SimpleData {
+    private int resId;
+    private String text;
+
+    public int getResId() {
+        return resId;
+    }
+
+    public void setResId(int resId) {
+        this.resId = resId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
